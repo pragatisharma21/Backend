16040677915c044f0c396dc9d7c0bc03ca88df16
pragatisharma21 <pragatis212@gmail.com>
2025-01-24 17:25:34 +0530

    Hospital data model generated..

---
+++ HospitalManagement/medicalrecord.model.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const medicalRecordSchema = new mongoose.Schema({},{})
+
+
+
+
+
+
+
+ export const MedicalRecord = mongoose.model("MedicalRecord", medicalRecordSchema)
